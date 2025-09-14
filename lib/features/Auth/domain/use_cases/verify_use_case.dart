6c4cff2a3218573a5f04b7551c2230e563e74694
Ramsi97 <ramadan2jemal@gmail.com>
2025-09-14 17:07:03 +0300

    domain layer is to be completed

---
+++ lib/features/Auth/domain/use_cases/verify_use_case.dart
@@ -0,0 +1,19 @@
+import 'package:chapp/core/error/failure.dart';
+import 'package:chapp/core/use_case/use_case.dart';
+import 'package:chapp/features/Auth/domain/entity/user_entity.dart';
+import 'package:chapp/features/Auth/domain/repository/auth_repository.dart';
+import 'package:dartz/dartz.dart';
+
+class Verify extends UseCase<UserEntity, VerifyParam> {
+  final AuthRepository repository;
+  Verify({required this.repository});
+  @override
+  Future<Either<Failure, UserEntity>> call(VerifyParam params) async {
+    return await repository.verify(params.otp);
+  }
+}
+
+class VerifyParam {
+  String otp;
+  VerifyParam({required this.otp});
+}
